bd3e4fbd32779c9eef313ce48b05395058a7e29d
Ankit kumar <ankitkumarcode24@gmail.com>
2023-11-15 22:10:04 +0530

    purchase-api

---
+++ app/api/api_v1/handlers/customer.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends
+from app.schemas.customer_schema import CustomerOut, CustomerCreate, CustomerUpdate
+from app.api.deps.retailer_deps import get_current_user
+from app.models.retailer_model import User
+from app.services.customer_service import CustomerService
+from app.models.customer_model import Customer
+from uuid import UUID
+from typing import List
+
+customer_router = APIRouter()
+
+@customer_router.get('/', summary="Get All Customers", response_model=List[CustomerOut])
+async def list(current_user: User = Depends(get_current_user)):
+    return await CustomerService.list_customers(current_user)
+
+@customer_router.post('/', summary="Create Customer", response_model=Customer)
+async def create_customer(data: CustomerCreate, current_user: User = Depends(get_current_user)):
+    return await CustomerService.create_customer(current_user,data)
+
+@customer_router.get('/{customer_id}', summary="Get a customer by customer_id", response_model=CustomerOut)
+async def retrieve(customer_id: UUID, current_user: User = Depends(get_current_user)):
+    return await CustomerService.retrieve_customer(current_user, customer_id)
+
+@customer_router.get('/retrieve/{phone_number}', summary="Get a customer by phone_number", response_model=CustomerOut)
+async def retrieve(phone_number: str, current_user: User = Depends(get_current_user)):
+    return await CustomerService.retrieve_customer2(current_user, phone_number)
+
+
+@customer_router.put('/{customer_id}', summary="Update customer by customer_id", response_model=CustomerOut)
+async def update(customer_id: UUID, data: CustomerUpdate, current_user: User = Depends(get_current_user)):
+    return await CustomerService.update_customer(current_user, customer_id, data)
+
+
+@customer_router.delete('/{customer_id}', summary="Delete customer by customer_id")
+async def delete(customer_id: UUID, current_user: User = Depends(get_current_user)):
+    await CustomerService.delete_customer(current_user, customer_id)
+    return None
